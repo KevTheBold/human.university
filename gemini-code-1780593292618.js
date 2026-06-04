ac60fcd564d60cbcd3be420b8090d3b4f2ea9335
KevTheBold <Kevin@sapient.city>
2026-06-04 10:16:45 -0700

    Add files via upload

---
+++ gemini-code-1780593292618.js
@@ -0,0 +1,9 @@
+export default {
+  logo: <span>Human.University</span>,
+  project: {
+    link: 'https://github.com/your-username/your-repo-name',
+  },
+  footer: {
+    text: '© 2026 Sapient City NPO',
+  },
+}
